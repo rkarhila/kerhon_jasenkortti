d07d3acad540b99cdc9746d03428d768a39690d0
Karhila Reima <rkarhila@t40511-da010.org.aalto.fi>
2015-03-27 17:31:33 +0200

    Alkaa jo hiljakseen toimia

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+Alppikerhon j&auml;senkorttigeneraattori
+<form action="generointiskripti_pdf.php" method="post">
+   S&auml;hk&ouml;postiosoite:<br>
+   <input type="text" name="email"><br>
+   Syntym&auml;vuosi:<br>
+   <input type="text" name="birthyear"><br>
+   <input type="submit" value="L&auml;het&auml;" name="submit">
+</form>
+
+</body>
+</html> 
